30140d2932c8173960771215991d20c32aabee0b
Mummalar Wilson <nmummalar@gmail.com>
2024-12-06 20:13:04 +0530

    small update

--- portfolio.js
+++ portfolio.js
@@ -8,3 +8,22 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+// Initialize EmailJS with your user ID
+emailjs.init('pvuLhbBzi5kyyLJSt'); // Replace 'YOUR_USER_ID' with your actual EmailJS user ID
+
+// Form submission event listener
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent the default form submission
+    
+    // Send email using EmailJS
+    emailjs.sendForm('service_n15w40c', 'template_euliiaq', this)
+        .then(function (response) {
+            alert('Email sent successfully!');
+            console.log('SUCCESS!', response.status, response.text);
+        }, function (error) {
+            alert('Failed to send email. Please try again.');
+            console.log('FAILED...', error);
+        });
+});
